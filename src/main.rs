6896a51ef13896eeade37ba5c09dc49c31db9b46
najme <bouachanajme@gmail.com>
2024-08-10 19:01:18 +0100

    creat fn

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,30 @@ use  std::env ;
 use std::fs ; 
 
 
+
+
 fn main () {
     let arg : Vec<String > = env::args().collect() ; 
     
-   
-    let  query :&String = &arg[1] ;
-    let  file_path : &String  = & arg[2] ;
-
-    println!("seraching for  {}" , query) ;
-    println!("in file path {}" , file_path) ;
-
-    println!("In file {file_path}");
-
-    let contents = fs::read_to_string(file_path)
+    let arg  = parse_config(&arg);
+        println!("seraching for  {}" , arg.file_path) ;
+    println!("in file path {}" , arg.query) ;
+    let contents = fs::read_to_string(tuple.1)
     .expect("Should have been able to read the file");
     println!("With text:\n{contents}");
 }
 
+struct args {
+    query  : String ,
+    file_path  : String ,
+}
+
+
+fn parse_config (arg : &[String]) -> args {
+    let  query :String = arg[1].clone() ;
+    let  file_path : String  =  arg[2].clone() ;
+    args {
+        query  , 
+        file_path  ,
+    }
+}
